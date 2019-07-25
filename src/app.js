4ab6d7d16f4164b8926ec0f363f57ab422e31f72
SolGee <solsv00@hotmail.com>
2019-07-25 00:47:22 -0500

    Funciones hide and show de las secciones

--- src/app.js
+++ src/app.js
@@ -7,7 +7,9 @@ const showSection = id => document.getElementById(id).classList.remove('hide');
 
 const signInButton = document.getElementById('btn-sign-in');
 const registerButton = document.getElementById('btn-register');
+const sendRegisterButton = document.getElementById('form-send');
 
+const homeButton = document.getElementById('btn-home');
 const incomesButton = document.getElementById('btn-incomes');
 const expensesButton = document.getElementById('btn-expenses');
 const balanceButton = document.getElementById('btn-balance');
@@ -21,3 +23,33 @@ const showHomeSection = () => {
 }
 
 signInButton.addEventListener('click', showHomeSection);
+homeButton.addEventListener('click', showHomeSection);
+
+const showHomeFromRegister = () => {
+    showSection ('home');
+    hideSection ('register-user');
+}
+
+sendRegisterButton.addEventListener('click', showHomeFromRegister);
+
+const showIncomesSection = () => {
+    showSection ('incomes');
+    hideSection ('home');
+}
+
+incomesButton.addEventListener('click', showIncomesSection);
+
+const showExpensesSection = () => {
+    showSection ('expenses');
+    hideSection ('incomes');
+}
+
+expensesButton.addEventListener('click', showExpensesSection);
+
+
+const showRegisterSection = () => {
+    hideSection ('login-user');
+    showSection ('register-user');
+}
+
+registerButton.addEventListener('click', showRegisterSection);
