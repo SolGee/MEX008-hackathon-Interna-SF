cf17b5f7bf2ab426d4604ae1da79006e1a45307c
Rosa Isela Lopez Palma <rosaiselalopezpalma@MacBook-Air-de-Rosa.local>
2019-07-25 11:46:22 -0500

    agregando función para loadEventListener y function getTasks en el archivo fromLocalStorage.js

--- src/fromLocalStorage.js
+++ src/fromLocalStorage.js
@@ -4,3 +4,31 @@ const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+  //DOM load event
+  document.addEventListener("DOMContentLoaded", getTasks);
+  // Add task event
+  form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+
+  // Clear task event
+  clearBtn.addEventListener('click', clearTasks);
+
+  // Filter tasks event
+  // filter.addEventListener('keyup', filterTasks);
+}
+
+// Get tasks from LS
+function getTasks(){
+  let tasks;
+  if(localStorage.getItem("tasks") === null){
+    tasks = [];
+  }else{
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
